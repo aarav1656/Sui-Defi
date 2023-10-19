cefdbe1f2001b36e013a3f7d1921335e6fd90fbe
Aarav raj <anshuk3917@gmail.com>
2023-10-19 06:17:49 +0000

    setting up UI for the Defi App

---
+++ frontend/src/routes/Home.tsx
@@ -0,0 +1,47 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useWalletKit } from '@mysten/wallet-kit';
+
+import { Loading } from '../components/Base/Loading';
+import { WalletNotConnected } from '../components/Base/WalletNotConnected';
+import { KioskCreation } from '../components/Kiosk/KioskCreation';
+import { KioskData } from '../components/Kiosk/KioskData';
+import { KioskSelector } from '../components/Kiosk/KioskSelector';
+import { useOwnedKiosk } from '../hooks/kiosk';
+import { useKioskSelector } from '../hooks/useKioskSelector';
+
+function Home() {
+	const { currentAccount } = useWalletKit();
+
+	const {
+		data: ownedKiosk,
+		isLoading,
+		refetch: refetchOwnedKiosk,
+	} = useOwnedKiosk(currentAccount?.address);
+
+	const { selected, setSelected, showKioskSelector } = useKioskSelector(currentAccount?.address);
+
+	// Return loading state.
+	if (isLoading) return <Loading />;
+
+	// Return wallet not connected state.
+	if (!currentAccount?.address) return <WalletNotConnected />;
+
+	// if the account doesn't have a kiosk.
+	if (!ownedKiosk?.kioskId) return <KioskCreation onCreate={refetchOwnedKiosk} />;
+
+	// kiosk management screen.
+	return (
+		<div className="container">
+			{showKioskSelector && selected && (
+				<div className="px-4">
+					<KioskSelector caps={ownedKiosk.caps} selected={selected} setSelected={setSelected} />
+				</div>
+			)}
+			{selected && currentAccount?.address && <KioskData kioskId={selected.kioskId} />}
+		</div>
+	);
+}
+
+export default Home;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		conditions: ['source'],
+	},
+});
